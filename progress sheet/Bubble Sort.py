3fbe88e294ad893bd080c9d6e2a20e2999f41929
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-06-23 17:46:39 +0300

    Update Bubble Sort.py

--- progress sheet/Bubble Sort.py
+++ progress sheet/Bubble Sort.py
@@ -7,16 +7,15 @@ import sys
 
 def countSwaps(a):
     # Write your code here
-    count=0
-    for i in range(n):
-        for j in range(n-1):
-            if a[j] > a[j + 1]:
-                a[j],a[j+1]=a[j+1],a[j]
-                count+=1
-    print("Array is sorted in"+" "+ str(count) +" "+"swaps.") 
-    print("First Element: " + str(a[0]) )
-    print("Last Element: " + str(a[-1]))  
-                
+    numSwaps=0
+    for i in range (len(a)):
+        for j in range (i+1,len(a)):
+            if a[j]<a[i]:
+                numSwaps+=1
+                a[i],a[j]=a[j],a[i]
+    print(f"Array is sorted in {numSwaps} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[-1]}")
 
 if __name__ == '__main__':
     n = int(input().strip())
