7569aec6a29a40bbcafb74017c2306f94cc44fbd
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-12-06 13:19:48 +0300

    Added solution for Multiply Strings

--- python-track/multiply-strings.py
+++ python-track/multiply-strings.py
@@ -1,5 +1,24 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        x=int(num1)*int(num2)
-        return str(x)
+        def convert(num):
+            num=num[::-1]
+            ans=0
+            for i in range(len(num)):
+                char=(ord(num[i])-48)*10**i
+                ans+=char
+            return ans
+        def revert(num):
+            ans=""
+            if num==0:
+                ans="0"
+            while num>0:
+                remainder=num%10
+                ans+=str(remainder)
+                num=num//10
+            return ans[::-1]
+
+        num1=convert(num1)
+        num2=convert(num2)
+        return revert(num1*num2)
+                
         
