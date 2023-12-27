761b0f95fd66010f34391acae1cbed49fb8bd503
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-12-27 18:01:51 +0300

    Added solution for Valid Sudoku

--- progress_sheet/valid-sudoku.py
+++ progress_sheet/valid-sudoku.py
@@ -1,30 +1,11 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         n=len(board)
-        c=list(zip(*board))
+        ans=[]
         for i in range(n):
-            s=set()
             for j in range(n):
-                if board[i][j]!='.' and board[i][j] in s:
-                    return False
-                s.add(board[i][j])
-        for i in range(n):
-            s=set()
-            for j in range(n):
-                if c[i][j]!='.' and c[i][j] in s:
-                    return False
-                s.add(c[i][j])
-        x=0
-        while x<9:
-            y=0
-            while y<9:
-                s=set()
-                for i in range(x,x+3):
-                    for j in range(y,y+3):
-                        if board[i][j]!='.' and board[i][j] in s:
-                            return False
-                        s.add(board[i][j])
-                y+=3
-            x+=3
-        return True
+                x=board[i][j]
+                if x !='.':
+                    ans+=[(i,x),(x,j),(i//3,j//3,x)]
+        return len(ans)==len(set(ans))
         
