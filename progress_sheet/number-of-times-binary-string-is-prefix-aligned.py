a25f737a9bb20d3371c79a514773f50ebd3c2916
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-12-27 19:19:09 +0300

    Added solution for Number of Times Binary String Is Prefix-Aligned

--- progress_sheet/number-of-times-binary-string-is-prefix-aligned.py
+++ progress_sheet/number-of-times-binary-string-is-prefix-aligned.py
@@ -4,8 +4,6 @@ class Solution:
         s=set()
         m=0
         for i in flips:
-            if i in s:
-                s.pop(i)
             if len(s)==m:
                 ans+=1
             s.add(i)
