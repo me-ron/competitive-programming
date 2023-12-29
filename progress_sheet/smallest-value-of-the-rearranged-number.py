50e940eb8976ade6732c312b5197afddb9f47950
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-12-29 13:46:25 +0300

    Added solution for Smallest Value of the Rearranged Number

---
+++ progress_sheet/smallest-value-of-the-rearranged-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestNumber(self, num: int) -> int:
+        if num>=0:
+            num=list(str(num))
+            num.sort()
+            i=0
+            while i<len(num):
+                if num[i]!='0':
+                    break
+                i+=1
+            if len(num)>1 and num[0]=='0':
+                num[0],num[i]=num[i],num[0]
+        else:
+            num=list(str(num))[1:]
+            num.sort(reverse=True)
+            num=['-']+num
+        return int("".join(num))
