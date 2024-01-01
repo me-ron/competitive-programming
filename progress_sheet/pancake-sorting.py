753c02821fdb5cf6ca8ad8c062fe34c2fe225711
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-01-01 13:53:42 +0300

    Added solution for Pancake Sorting

--- progress_sheet/pancake-sorting.py
+++ progress_sheet/pancake-sorting.py
@@ -1,7 +1,11 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         def flip(arr,i):
-            arr[:i+1]=arr[:i+1][::-1]
+            j=0
+            while j<i:
+                arr[i],arr[j]=arr[j],arr[i]
+                j+=1
+                i-=1
 
         ans = []
         j=len(arr)-1
