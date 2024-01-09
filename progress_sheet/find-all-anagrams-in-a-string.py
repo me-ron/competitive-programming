fd9f16e1584eea794737d9572a656703aefc6eee
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-01-09 16:28:07 +0300

    Added solution for Find All Anagrams in a String

--- progress_sheet/find-all-anagrams-in-a-string.py
+++ progress_sheet/find-all-anagrams-in-a-string.py
@@ -5,8 +5,7 @@ class Solution:
         c2 = defaultdict(int)
         i = 0
         for j in range(len(s)):
-            if s[j] in c1:
-                c2[s[j]] += 1
+            c2[s[j]] += s[j] in c1
             if j - i + 1 > len(p):
                 c2[s[i]] -= s[i] in c1
                 if c2[s[i]] == 0:
