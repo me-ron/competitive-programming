7ca53fe7e7023c613239f4152b8158af542f3c32
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-01-13 21:57:02 +0300

    Added solution for Minimum Window Substring

--- progress_sheet/minimum-window-substring.py
+++ progress_sheet/minimum-window-substring.py
@@ -2,16 +2,19 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         t=Counter(t)
         i=0
-        ans=s+"rew"
+        dis=len(s)+3
+        x,y=0,0
         for j in range(len(s)):
             if s[j] in t:
                 t[s[j]]-=1
             while all([t[ch]<=0 for ch in t]) and i<len(s):
-                ans=min(ans,s[i:j+1],key=len)
+                if dis>j-i+1:
+                    dis=j-i+1
+                    x,y=i,j
                 if s[i] in t:
                     t[s[i]]+=1
                 i+=1
-        return ans if len(ans)<=len(s) else ""
+        return s[x:y+1] if dis<=len(s) else ""
                     
         
         
