3ce3b6d2ffc610361af9447722ee67cac807df86
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-01-13 22:10:07 +0300

    Added solution for Minimum Window Substring

--- progress_sheet/minimum-window-substring.py
+++ progress_sheet/minimum-window-substring.py
@@ -8,16 +8,14 @@ class Solution:
         for j in range(len(s)):
             if s[j] in t:
                 t[s[j]] -= 1
-                if t[s[j]] == 0:
-                    required -= 1
+                required -= t[s[j]] == 0
             while required == 0:
                 if dis > j - i + 1:
                     dis = j - i + 1
                     x, y = i, j
                 if s[i] in t:
                     t[s[i]] += 1
-                    if t[s[i]] > 0:
-                        required += 1
+                    required += t[s[i]] > 0
                 i += 1
         return s[x:y+1] if dis <= len(s) else ""
                     
