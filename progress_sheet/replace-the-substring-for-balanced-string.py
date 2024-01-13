6c2d503c40f14b413b05490571884b544896b96a
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-01-13 19:13:42 +0300

    Added solution for Replace the Substring for Balanced String

--- progress_sheet/replace-the-substring-for-balanced-string.py
+++ progress_sheet/replace-the-substring-for-balanced-string.py
@@ -2,14 +2,14 @@ class Solution:
     def balancedString(self, s: str) -> int:
         n = len(s)//4
         left, ans, count = 0,float('inf'), Counter(list(s))
-        count={ch:count[ch]-n for ch in count if count[ch]>n}
+        have={ch:count[ch]-n for ch in count if count[ch]>n}
         
         for right in range(len(s)):
-            if s[right] in count:
-                count[s[right]] -= 1
-            while all(value <= 0 for value in count.values()) and left < len(s):
-                if  s[left] in count:
-                    count[s[left]] += 1
+            if s[right] in have:
+                have[s[right]] -= 1
+            while all(value <= 0 for value in have.values()) and left < len(s):
+                if  s[left] in have:
+                    have[s[left]] += 1
                 ans = min(ans, right - left + 1)
                 left += 1
                
