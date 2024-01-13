dc2eea33aa4dc25d7896240f8f4b0c1da13e059f
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-01-13 19:25:58 +0300

    Added solution for Longest Repeating Character Replacement

--- progress_sheet/longest-repeating-character-replacement.py
+++ progress_sheet/longest-repeating-character-replacement.py
@@ -1,10 +1,8 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         dic=defaultdict(int)
-        i=0
-        j=0
-        ans=0
-        max_len=0
+        i=ans=max_len=0
+        
         for j in range(len(s)):
             dic[s[j]]+=1
             max_len=max(max_len,dic[s[j]])
