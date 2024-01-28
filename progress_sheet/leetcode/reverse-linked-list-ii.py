be6efd579b1427f4baca6a375aea784d229cdafc
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-01-28 16:55:59 +0300

    Add solution for Reverse Linked List II

--- progress_sheet/leetcode/reverse-linked-list-ii.py
+++ progress_sheet/leetcode/reverse-linked-list-ii.py
@@ -5,26 +5,25 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        bl, ar = None, None
-        l, r = None, None
-        dummy = head
-        idx = 1
+        bl,ar=None,None
+        l,r=None,None
+        dummy=head
+        idx=1
         while dummy:
-            if idx == left - 1: bl = dummy
-            elif idx == left:
-                l = curr = dummy
-                prev = None
-                while curr and idx <= right:
-                    if idx == right: r = curr
-                    temp = curr.next
-                    curr.next = prev
-                    prev = curr
-                    curr = temp
-                    idx += 1
-                l.next = curr
-            dummy = dummy.next
-            idx += 1
-
-        if bl: bl.next = r
-        else: head = r
+            if idx==left-1:bl=dummy
+            elif idx==left:
+                l=curr=dummy
+                prev=None
+                while curr and idx<=right:
+                    if idx==right:r=curr
+                    temp=curr.next
+                    curr.next=prev
+                    prev=curr
+                    curr=temp
+                    idx+=1
+                l.next=curr
+            dummy=dummy.next
+            idx+=1
+        if bl:bl.next=r
+        else: head=r
         return head
