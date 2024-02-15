062c1faa68c5f72edfd54074c206cfb3d5143234
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-02-15 18:54:05 +0300

    Add solution for Valid Triangle Number

--- progress_sheet/leetcode/valid-triangle-number.py
+++ progress_sheet/leetcode/valid-triangle-number.py
@@ -1,13 +1,15 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        ans = 0
         nums.sort()
-        count = 0
-        for i in range(len(nums) - 1, 1, -1):
-            left, right = 0, i - 1
-            while left < right:
-                if nums[left] + nums[right] > nums[i]:
-                    count += right - left
-                    right -= 1
+        for i in range(len(nums)-1,1,-1):
+            l, r = 0, i-1
+            while l < r:
+                if nums[i] < nums[l] + nums[r]:
+                    ans += r - l
+                    r -= 1
                 else:
-                    left += 1
-        return count
+                    l += 1
+        return ans
+        
+        
