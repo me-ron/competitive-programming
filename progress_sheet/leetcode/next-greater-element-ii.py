9a7416b30e5fe0aff9494ed2a15605579d62a3c7
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-02-22 14:28:34 +0300

    Add solution for Next Greater Element II

---
+++ progress_sheet/leetcode/next-greater-element-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack = []
+        n = len(nums)
+        ans = [-1]*n
+        for i in range(2*n - 1, -1, -1):
+            while stack and stack[-1] <= nums[i%n]:
+                stack.pop()
+            if ans[i%n] == -1:
+                ans[i%n] = stack[-1] if stack else -1
+            stack.append(nums[i%n])
+            
+        return ans
