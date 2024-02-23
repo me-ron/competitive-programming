007bcb2db5c47dbe8fce7b46566dff0b398064d4
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-02-23 10:00:40 +0300

    Add solution for Sum of Subarray Minimums

---
+++ progress_sheet/leetcode/sum-of-subarray-minimums.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        arr = [0] + arr
+        stack = [0]
+        ans = [0] * len(arr)
+
+        for i in range(len(arr)):
+            while arr[stack[-1]] > arr[i]:
+                stack.pop()
+            j = stack[-1]
+            ans[i] = ans[j] + (i-j)*arr[i]
+            stack.append(i)
+        
+        return sum(ans) % (10**9 + 7)
