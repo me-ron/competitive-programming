72709aa2582e56ef8a8f0cee99fc93b1f9f4a9eb
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-02-28 13:51:02 +0300

    Add solution for Predict the Winner

--- progress_sheet/leetcode/predict-the-winner.py
+++ progress_sheet/leetcode/predict-the-winner.py
@@ -3,14 +3,17 @@ class Solution:
         def score(arr, l, r, player1):
             if r - l == 0:
                 return arr[l]
+
             if player1:
                 left = arr[l] + score(arr, l + 1, r, 0)
                 right = arr[r] + score(arr, l, r - 1, 0)
                 return max(left,right)
+                
             else:
                 left = score(arr, l + 1, r, 1)
                 right = score(arr, l, r - 1, 1) 
                 return min(left, right)
+
         player1 = score(nums, 0, len(nums) - 1, 1)
         player2 = sum(nums) - player1
         return player1 >= player2 
