9f498253fd674aec862617732371bc1c483b11d8
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-03-03 00:20:08 +0300

    Add solution for Maximum Sum BST in Binary Tree

---
+++ progress_sheet/leetcode/maximum-sum-bst-in-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxSumBST(self, root: Optional[TreeNode]) -> int:
+        def helper(root, min_val, max_val):
+            if not root:
+                return (inf,-inf, 0)
+            
+            left_min, left_max, left_sum = helper(root.left, min_val, root.val)
+            right_min, right_max, right_sum = helper(root.right, root.val, max_val)
+            
+            if left_max < root.val < right_min:
+                curr_sum = left_sum + right_sum + root.val
+                self.result = max(self.result, curr_sum)
+                return (min(left_min, root.val), max(right_max, root.val), curr_sum)
+            else:
+                return (-inf, inf, 0)
+        
+        self.result = 0
+        helper(root, -inf, inf)
+        return self.result
