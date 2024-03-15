1e8284afe7c0b521631af5b36462b60f58f799f1
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-03-15 16:51:39 +0300

    Add solution for Dota2 Senate

---
+++ progress_sheet/leetcode/dota2-senate.py
@@ -0,0 +1,27 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        c = Counter(senate)
+        queue = deque(senate)
+
+        red, die = 0, 0
+        while c["R"] and c["D"]:
+            vote = queue.popleft()
+            if vote == 'R':
+                if red >= 0:
+                    die -= 1
+                    c['D'] -= c['D'] > 0
+                    queue.append(vote)
+                else:
+                    red += 1
+
+            else:
+                if die >= 0:
+                    red -= 1
+                    c['R'] -= c['R'] > 0
+                    queue.append(vote)
+                else:
+                    die += 1
+            
+        if not c['R']:
+            return 'Dire'
+        return 'Radiant'
